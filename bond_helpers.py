84562e3ee47063f28ebcb46f05d446b7c767d340
Bay Foley-Cox <bayfoleycox@gmail.com>
2023-12-05 17:50:45 +0000

    charge mask

--- bond_helpers.py
+++ bond_helpers.py
@@ -22,6 +22,7 @@ def get_mols(charges, bonds, node_mask):
 
 def get_mol(adj, charges, n_atoms):
     adj = adj.argmax(dim=-1)[:n_atoms, :n_atoms]
+    charges = charges[:n_atoms]
     if charges.min() < 1 or charges.max() > 9:
         print("charge error: ", charges.min(), " max: ", charges.max())
         return None
